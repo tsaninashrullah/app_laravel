cd6648379916f8448c8ca71ec5cba582eb7df4cb
Tsani Nashrullah <tsani.nashrullah@gmail.com>
2016-09-15 13:16:13 +0700

    Make a class from css for message fix bugs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,33 +1,35 @@
 @extends('layout.app')
 
 @section('content')
-<div class="content">
 <center><h1>My Profile</h1></center>
-<div class="col s4">
-    <strong><h4>Tsani Nashrullah</h4></strong>
-    <img src="images/avatar.jpg" class="responsive-img circle" width="50%">
+<div class="row">
+    <div class="col l4">
+        <strong><h4>Tsani Nashrullah</h4></strong>
+        <img src="images/avatar.jpg" class="responsive-img circle" width="50%">
+    </div>
+    <div class="col l8">
+        <table class="responsive-table highlight bordered">
+            <tr>
+                <th><p>Nama</th>
+                <td>Tsani Nashrullah</td>
+            </tr>
+            <tr>
+                <th>Umur</th>
+                <td>18 Tahun</td>
+            </tr>
+            <tr>
+                <th>Alamat</th>
+                <td>Cimahi</td>
+            </tr>
+            <tr>
+                <th>Hobi</th>
+                <td>Futsal</p></td>
+            </tr>
+        </table>
+    </div>
 </div>
-<div class="col s8">
-    <table class="responsive-table highlight bordered">
-        <tr>
-            <th><p>Nama</th>
-            <td>Tsani Nashrullah</td>
-        </tr>
-        <tr>
-            <th>Umur</th>
-            <td>18 Tahun</td>
-        </tr>
-        <tr>
-            <th>Alamat</th>
-            <td>Cimahi</td>
-        </tr>
-        <tr>
-            <th>Hobi</th>
-            <td>Futsal</p></td>
-        </tr>
-    </table>
-</div>
-    <div class="col s12">
+<div class="row">
+    <div class="col l12">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
